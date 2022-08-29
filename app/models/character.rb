c4d99dcf2d5405e3c3100068d12011897559cc98
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-08-29 16:04:58 +0300

    commit tree e454d2b975602f726b7cef2cd85e2bbfddd78047

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,14 @@
 class Character < ActiveRecord::Base
+
+    def actor 
+        self.actor
+    end
   
+    def show 
+        self.show
+    end
+
+    def say_that_thing_you_say 
+        "#{self.name} always say: #{self.catchphrase}"
+    end
 end
--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -1,5 +1,7 @@
 class AddCatchphraseToCharacters < ActiveRecord::Migration[6.1]
   def change
     # Your code here
+    add_column :characters, :catchphrase, :string
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
   end
 end
